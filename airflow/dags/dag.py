5e870a4f27d57d4d7d46e8d92ec7b9ababe6a0c8
robert-mitosinka <robert.mitosinka@gmail.com>
2025-06-16 17:35:15 +0200

    Add Airflow configuration files, implement quicksort in Python and C++, and create test data generation script

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -5,22 +5,26 @@ from datetime import datetime
 import sys
 
 
-sys.path.append('/home/robert/construo/airflow')  # Update this path
+sys.path.append("/home/robert/construo/airflow")  # Update this path
 
-from ..app.py import main_run
+from ..app.py import quick_sort
+
+main_run = quick_sort
 
 default_args = {
-    'start_date': datetime(2025, 6, 16),
+    "start_date": datetime(2025, 6, 16),
 }
 
-with DAG('simple_python_app',
-         schedule_interval='@daily',
-         catchup=False,
-         default_args=default_args,
-         description='Run a simple Python script with Airflow') as dag:
+with DAG(
+    "dag_numero_1",
+    schedule_interval="@daily",
+    catchup=False,
+    default_args=default_args,
+    description="Run a simple Python script with Airflow",
+) as dag:
 
     run_python_app = PythonOperator(
         task_id='run_my_script',
         python_callable=main_run,
-        op_args=['in_array', [12, 47, 3, 89, 25, 61, 34, 78, 56, 90, 11, 42, 67, 8, 29, 73, 5, 38, 94, 21]]
+        op_args=['in_array', [12, 47, 3, 89, 25, 61, 34, 78, 56, 90, 11, 42, 67, 8, 29, 73, 5, 38, 94, 21]]  # fmt: skip
     )
